006b28968114272524955e636157186a74be2f38
crillo1 <88744288+crillo1@users.noreply.github.com>
2022-05-10 23:07:52 +0200

    ,

---
+++ src/Home/components/ContractInfo.js
@@ -0,0 +1,43 @@
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import treasuryImg from "../../assets/treasury.png"
+import profitImg from "../../assets/profit.png"
+import { styled } from "@mui/system";
+
+const CardWrapper = styled(Card)({
+  background: "rgb(255 252 248)",
+  marginBottom: 24,
+});
+
+const nutritionFacts = [
+  {
+    label: "Daily Return",
+    value: 10,
+  },
+  {
+    label: "APR",
+    value: "3,650",
+  },
+  {
+    label: "Dev Fee",
+    value: 4.5, 
+  },
+];
+
+export default function ContractInfo() {
+  return (
+    <div >
+      <div style={{display: "flex"}}>
+        <img src={treasuryImg}></img>
+        <div>
+          <h5>Contract balance</h5>
+          <h6>2000 AVAX</h6>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  chainId: 97,
+  contractAddress: "0xa824d59615ec4eb6228feb55a07a6437f17de7d6",
+  scanLink:
+    "https://testnet.bscscan.com/address/0xa824d59615ec4eb6228feb55a07a6437f17de7d6"
+};
